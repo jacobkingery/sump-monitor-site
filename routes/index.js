715f6da804027c5297e4fa6d907563f0a8d20371
Jacob Kingery <jacob.kingery@students.olin.edu>
2015-01-17 22:54:59 -0600

    added capability of seeing last 5 readings

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ var fs = require('fs');
 exports.index = function(req, res){
   fs.readFile('state.json', function (err, data) {
     if (err) throw err;
-    obj = JSON.parse(data);
-    var level = obj.level;
-    var timestamp = obj.timestamp;
 
-    res.render('index', { title: 'Sump Monitor', timestamp: timestamp, level: level });
+    obj = JSON.parse(data);
+    var readings = obj.readings;
+
+    res.render('index', { title: 'Sump Monitor', readings: readings.reverse() });
   });
 };
 
@@ -24,13 +24,22 @@ exports.index = function(req, res){
 exports.update = function(req, res){
   var level = req.body.level;
   var timestamp = req.body.timestamp;
-  fs.writeFile('state.json', JSON.stringify({ timestamp: timestamp, level: level }), function(err,data) {
-    res.setHeader('Content-Type', 'application/json');
-    if (err) {
-      res.end(JSON.stringify({ success: false }));
-    } else {
-      res.end(JSON.stringify({ success: true }));
-    }
+  fs.readFile('state.json', function (err, data) {
+    if (err) throw err;
+    
+    obj = JSON.parse(data);
+    var readings = obj.readings;
+    readings = readings.slice(1)
+    readings.push([timestamp, level]);
+    
+    fs.writeFile('state.json', JSON.stringify({ readings: readings }), function(err,data) {
+      res.setHeader('Content-Type', 'application/json');
+      if (err) {
+        res.end(JSON.stringify({ success: false }));
+      } else {
+        res.end(JSON.stringify({ success: true }));
+      }
+    });
   });
 };
 
