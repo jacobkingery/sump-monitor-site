d0ea8b7319b6dc79ba4bba3898730d12b071c81a
Jacob Kingery <jacob.kingery@students.olin.edu>
2015-12-28 20:49:35 -0600

    added graph and live-updating

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -27,9 +23,50 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.post('/update', routes.update);
+app.get('/', function(req, res) {
+  fs.readFile('state.json', function(err, data) {
+    if (err) throw err;
 
-http.createServer(app).listen(app.get('port'), function(){
+    obj = JSON.parse(data);
+    var readings = obj.readings;
+
+    res.render('index', {
+      title: 'Sump Monitor',
+      readings: readings.reverse()
+    });
+  });
+});
+
+app.post('/update', function(req, res) {
+  var reading = {
+    Time: req.body.timestamp,
+    Level: req.body.level
+  };
+
+  fs.readFile('state.json', function(err, data) {
+    if (err) throw err;
+    
+    // Remove oldest reading, add new reading
+    obj = JSON.parse(data);
+    var readings = obj.readings;
+    readings = readings.slice(1)
+    readings.push(reading);
+
+    // Write readings to file
+    fs.writeFile('state.json', JSON.stringify({ readings: readings }), function(err,data) {
+      res.setHeader('Content-Type', 'application/json');
+      if (err) {
+        res.end(JSON.stringify({ success: false }));
+      } else {
+        res.end(JSON.stringify({ success: true }));
+      }
+    });
+
+    // Emit updated readings array to listening sockets
+    io.emit('update', readings.reverse());
+  });
+});
+
+http.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
